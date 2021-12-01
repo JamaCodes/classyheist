0077b71e9b30d2ca754af1b1022ce46ce92fb255
JamaCodes <jama.avwork@gmail.com>
2021-12-01 14:55:37 -0600

    bank and robbers created w/ Irobber

---
+++ Muscle.cs
@@ -0,0 +1,16 @@
+using system;
+
+namespace ClassyHeist
+{
+    public class Muscle : IRobber
+    {
+        public string Name {get; set;}
+        public int SkillLevel {get ; set;}
+        public int PercentageCut {get ; set;}
+
+        public void PerformSkill(bank bank)
+        {
+            bank.AlarmScore = bank.AlarmScore - SkillLevel;
+        }
+    }
+}
